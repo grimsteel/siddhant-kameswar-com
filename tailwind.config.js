58d7eb6b632ce92f4812a0a93f829ae004d7f64e
Siddhant Kameswar <115331356+grimsteel@users.noreply.github.com>
2024-06-23 02:07:29 -0500

    Add orange tinge to gray colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,20 @@ module.exports = {
     extend: {
       colors: {
         primary: colors.amber,
-        gray: colors.zinc
+        //gray: colors.zinc,
+        gray: {
+          50: "#f3e9e1",
+          100: "#ede3dc",
+          200: "#dfd5d0",
+          300: "#d0c6c2",
+          400: "#a39999",
+          500: "#786e6e",
+          600: "#5c5252",
+          700: "#4b413f",
+          800: "#352b26",
+          900: "#281e18",
+          950: "#1a100a"
+        }
       },
       fontFamily: {
         'sans': ['"Fira Code"', ...defaultTheme.fontFamily.sans]
