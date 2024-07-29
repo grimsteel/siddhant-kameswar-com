b200a9f13b8d2f7f717829a3d3d4e7efd0ca6035
Siddhant Kameswar <115331356+grimsteel@users.noreply.github.com>
2024-07-29 01:57:00 -0500

    homepage projects and openwrt screenshot

---
+++ src/index.11tydata.js
@@ -0,0 +1,66 @@
+module.exports = {
+  eleventyComputed: {
+    sections(data) {
+      return data.rawSections.map(({ projects, cat }) => ({
+        cat,
+        projects: projects.map(({ cat, idx }) => {
+          return data.projects[cat][idx];
+        })
+      }));
+    }
+  },
+  rawSections: [
+    {
+      "projects": [
+        {
+          "cat": "web-development",
+          "idx": 7
+        },
+        {
+          "cat": "web-development",
+          "idx": 3
+        }
+      ],
+      "cat": "web-development"
+    },
+    {
+      "projects": [
+        {
+          "cat": "desktop-app-development",
+          "idx": 1
+        },
+        {
+          "cat": "desktop-app-development",
+          "idx": 8
+        }
+      ],
+      "cat": "desktop-app-development"
+    },
+    {
+      "projects": [
+        {
+          "cat": "android-app-development",
+          "idx": 2
+        },
+        {
+          "cat": "android-app-development",
+          "idx": 7
+        }
+      ],
+      "cat": "android-app-development"
+    },
+    {
+      "projects": [
+        {
+          "cat": "network-administration",
+          "idx": 5
+        },
+        {
+          "cat": "system-administration",
+          "idx": 5
+        }
+      ],
+      "cat": "network-administration"
+    }
+  ]
+};
